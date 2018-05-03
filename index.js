a2052e3fe95c53a0d6f40300d98c8c924bd4ca81
Henriette Steenhoff <henriette.steenhoff@gmail.com>
2018-05-03 15:14:50 +0200

    Changed to fit our styling

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin = {top: 20, right: 20, bottom: 40, left: 50},
+    width = 800 - margin.left - margin.right,
+    height = 250 - margin.top - margin.bottom,
+    max_bgl_value = 21,
+    kidsAge = 10,
+    high_threshold = 10,
+    low_threshold = 6;
+
+if (kidsAge > 12) {
+    high_threshold = 12;
+    low_threshold = 8;
+} else {
+    high_threshold = 10;
+    low_threshold = 6;
+}
+console.log("kids age: " + kidsAge + "\n" + "thres_low: " + low_threshold + "\n" + "thres_high: " + high_threshold);
 
 // parse the date / time
-// var parseTime = d3.utcParse("%Y-%m-%dT%H:%M:%SZ");
 var parseDate = d3.timeParse("%Y-%m-%d %H:%M:%S");
-var formatTime = d3.timeFormat("%e %B");
+// %e space padded day of month
+// %B full month name
+var formatTime = d3.timeFormat("%e/%B");
 console.log(parseDate("2017-09-11 18:35:10"))
+
 // set the ranges
 var x = d3.scaleTime().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
@@ -17,32 +32,17 @@ var div = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-
-var tempprobe_line = d3.line()
+var calculated_bgl_line = d3.line()
     .x( function(d) { return x(d.timestamp); })
-    .y( function(d) { return y(d.tempprobe); });
+    .y( function(d) { return y(d.calculated_bgl); });
 
 var high_threshold_line = d3.line()
     .x(function(d){ return x(d.timestamp); })
-    .y(function(d){ return y(d.threshold_high); });
+    .y(function(d){ return y(high_threshold); });
 
 var low_threshold_line = d3.line()
-    .x(function(d){
-        return x(d.timestamp);
-    })
-    .y(function(d){
-        return y(d.threshold_low);
-    })
-
-
-var ambient_line = d3.line()
-    .x(function(d)
-        { return x(d.timestamp);}
-    )
-    .y( function(d) {
-        return y(d.ambient);
-    });
-
+    .x(function(d){ return x(d.timestamp); })
+    .y(function(d){ return y(low_threshold); })
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -59,8 +59,7 @@ function draw(data, tempdata) {
     data.forEach(function(d, i) {
 
         d.timestamp = parseDate(d.timestamp);
-        d.tempprobe = +d.tempprobe;
-        d.ambient = +d.ambient;
+        d.calculated_bgl = +d.calculated_bgl;
 
     });
 
@@ -75,20 +74,14 @@ function draw(data, tempdata) {
         return d.timestamp;
     }));
     y.domain([0, d3.max(data, function(d){
-        return Math.max(d.tempprobe, d.ambient);
+        return Math.max(d.calculated_bgl, max_bgl_value);
     })]);
 
-    // Add the tempprobe path.
+    // Add the calculated_bgl path.
     svg.append("path")
         .data([data])
         .attr("class", "line temp-probe temperature")
-        .attr("d", tempprobe_line);
-
-    // Add the ambient path
-    svg.append("path")
-        .data([data])
-        .attr("class", "line ambient temperature")
-        .attr("d", ambient_line);
+        .attr("d", calculated_bgl_line);
 
     svg.append("path")
         .data([data])
@@ -101,17 +94,31 @@ function draw(data, tempdata) {
         .attr("d", low_threshold_line)
 
 
-
-
     // add the X Axis
     svg.append("g")
         .attr("transform", "translate(0,"+ height + ")")
         .call(d3.axisBottom(x));
 
+    // text label for the x axis
+      svg.append("text")             
+          .attr("transform",
+                "translate(" + (width/2) + " ," + 
+                               (height + margin.top + 10) + ")")
+          .style("text-anchor", "middle")
+          .text("Time of day");
+
     // add the Y Axis
     svg.append("g")
         .call(d3.axisLeft(y));
-
+     
+    // text label for the y axis
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1.75em")
+        .style("text-anchor", "middle")
+        .text("Glucose level (mmol/L"); 
 }
 
 d3.json("temp_data.json",
